dfe3f87a4bcfc7231f417ca106060ca3ac043218
Songruoning <songruoning@yeah.net>
2021-05-17 03:17:30 +0000

    add offer09

---
+++ javaTest/src/offer09.java
@@ -0,0 +1,39 @@
+import java.util.LinkedList;
+import java.util.Stack;
+
+public class offer09 {
+    /**
+     * 剑指 Offer 09. 用两个栈实现队列
+     * https://leetcode-cn.com/problems/yong-liang-ge-zhan-shi-xian-dui-lie-lcof/
+     * 
+     * @param strings
+     */
+    public static void main(String[] strings) {
+
+    }
+
+    Stack<Integer> in = null;
+    Stack<Integer> out = null;
+
+    public CQueue() {
+        this.in = new Stack<Integer>();
+        this.out = new Stack<Integer>();
+    }
+
+    public void appendTail(int value) {
+        this.in.push(value);
+    }
+
+    public int deleteHead() {
+        if (this.out.empty()) {
+            while (!this.in.empty()) {
+                this.out.push(this.in.pop());
+            }
+        }
+        if (this.out.empty()) {
+            return -1;
+        } else {
+            return this.out.pop();
+        }
+    }
+}
