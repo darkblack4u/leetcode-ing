19a7dc3ffe3575388f527efbc19ab2e6b4207a93
Songruoning <songruoning@yeah.net>
2021-05-21 10:19:02 +0000

    update

---
+++ javaTest/src/offer52.java
@@ -0,0 +1,36 @@
+public class offer52 {
+    /**
+     * 剑指 Offer 52. 两个链表的第一个公共节点
+     * https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
+     * 
+     * @param strings
+     */
+    public static void main(String[] strings) {
+
+    }
+
+    /**
+     * 指针过两次，会在公共节点集合如果没有一样的，就都指null
+     * 
+     * @param headA
+     * @param headB
+     * @return
+     */
+    public static ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode a = headA;
+        ListNode b = headB;
+        while (a != b) {
+            if (a == null) {
+                a = headB;
+            } else {
+                a = a.next;
+            }
+            if (b == null) {
+                b = headA;
+            } else {
+                b = b.next;
+            }
+        }
+        return a;
+    }
+}
