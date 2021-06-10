615691f0e115c748e8ec14670fa26b02ac9d342e
Songruoning <songruoning@yeah.net>
2021-06-10 09:18:52 +0000

    每日一题

---
+++ javaTest/src/lc518.java
@@ -0,0 +1,22 @@
+public class lc518 {
+    /**
+     * 518. 零钱兑换 II
+     * 
+     * 给定不同面额的硬币和一个总金额。写出函数来计算可以凑成总金额的硬币组合数。假设每一种面额的硬币有无限个。
+     * https://leetcode-cn.com/problems/coin-change-2/
+     * 
+     * @param amount
+     * @param coins
+     * @return
+     */
+    public int change(int amount, int[] coins) {
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
+    }
+}
