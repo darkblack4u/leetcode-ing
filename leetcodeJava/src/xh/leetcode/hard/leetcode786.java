c79dc87465c3ffc18197a7b10b4fbbeca13e8676
Songruoning <songruoning@yeah.net>
2021-11-29 22:33:04 +0800

    update

--- leetcodeJava/src/xh/leetcode/hard/leetcode786.java
+++ leetcodeJava/src/xh/leetcode/hard/leetcode786.java
@@ -1,5 +1,7 @@
 package xh.leetcode.hard;
 
+import java.util.*;
+
 public class leetcode786 {
     public static void main(String[] args) {
         int[] arr = new int[] { 1, 2, 3, 5 };
@@ -11,15 +13,15 @@ public class leetcode786 {
         int n = arr.length;
         PriorityQueue<int[]> pq = new PriorityQueue<int[]>((x, y) -> arr[x[0]] * arr[y[1]] - arr[y[0]] * arr[x[1]]);
         for (int j = 1; j < n; ++j) {
-            pq.offer(new int[]{0, j});
+            pq.offer(new int[] { 0, j });
         }
         for (int i = 1; i < k; ++i) {
             int[] frac = pq.poll();
             int x = frac[0], y = frac[1];
             if (x + 1 < y) {
-                pq.offer(new int[]{x + 1, y});
+                pq.offer(new int[] { x + 1, y });
             }
         }
-        return new int[]{arr[pq.peek()[0]], arr[pq.peek()[1]]};
+        return new int[] { arr[pq.peek()[0]], arr[pq.peek()[1]] };
     }
 }
